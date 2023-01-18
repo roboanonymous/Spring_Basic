7f90b1e7dfebbd82bbf607743820ba8c1250309d
roboanonymous <jsamyak591@gmail.com>
2023-01-18 17:03:01 +0530

    first commit

---
+++ src/main/java/com/samyak/Spring/App.java
@@ -0,0 +1,13 @@
+package com.samyak.Spring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
